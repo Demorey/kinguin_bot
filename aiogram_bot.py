4083295923c5c0fd434ba57366c9b51b65e72871
Demorey <demorey2@gmail.com>
2024-05-29 13:42:44 +0300

    updated bot start with aiogram 3

--- aiogram_bot.py
+++ aiogram_bot.py
@@ -1,16 +1,37 @@
 import asyncio
-from aiogram.utils import executor
-import handlers
-from create_bot import dp
+import os
+import logging
+from handlers.client import task
+from create_bot import dp, bot
+from logging.handlers import RotatingFileHandler
+
+if not os.path.isdir("logs/"):
+    os.makedirs("logs/")
+if not os.path.exists("logs/debug_loger.log"):
+    open('logs/debug_loger.log', 'a').close()
+
+logging.basicConfig(level=logging.DEBUG, encoding='utf-8',
+                    handlers=[RotatingFileHandler('logs/debug_loger.log', maxBytes=5000000, backupCount=1)],
+                    format="%(asctime)s - [%(levelname)s] - %(funcName)s: %(lineno)d - %(message)s")
+
+async def bot_start():
+    try:
+        bot_name = await bot.get_me()
+        print('Запущен бот: ' + bot_name.first_name)
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 
-async def on_startup(_):
-    print('Бот запущен')
+async def main():
+    tasks = [bot_start()
+             # , task()
+             ]
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.create_task(handlers.client.task())
-
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped!')
--- create_bot.py
+++ create_bot.py
@@ -1,13 +1,12 @@
 from aiogram import Bot
 import os
 from dotenv import load_dotenv
-from aiogram.dispatcher import Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Dispatcher
 
 load_dotenv()
 
 bot = Bot(token=os.getenv("BOT_TOKEN"))
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot=bot)
 chat_id_list = os.getenv("CHAT_ID").split(',')
 chat_id_list = list(map(int, chat_id_list))
 TIMER = int(os.getenv("TIMER"))
